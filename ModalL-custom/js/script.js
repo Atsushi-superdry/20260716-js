d079ea20c3113e90dcff97be23832a6d09f2c3e8
Atsushi-superdry <TTC2550DT0007@edu.tech.ac.jp>
2026-07-16 15:12:01 +0900

    Add files via upload

---
+++ ModalL-custom/js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+  $("a").click(function() {
+    $("body")
+      .append('<div id="bg">')
+      .append('<div id="photo">');
+
+      $("#bg, #photo").hide();
+      $("#photo").html("<img>");
+      $("#photo img")
+        .attr("src", $(this).attr("href"))
+        .attr("alt", "Photo");
+      $("#bg, #photo").fadeIn(800); 
+
+      $("#bg").click(function() {
+        $(this).fadeOut(function() {
+          $(this).remove();
+        });
+
+        $("#photo").fadeOut(function() {
+          $(this).remove();
+        });
+      });
+
+    return false;
+  });
+});
